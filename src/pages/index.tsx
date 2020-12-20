cd9ebfdca5dbc30866dfcb85364ed772c60aaa19
Mayro <deehmyller11@gmail.com>
2020-12-20 01:25:52 -0300

    Add eslint configuring rules and plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const foo = 'bar';
+
   return (
     <div className={styles.container}>
       <Head>
